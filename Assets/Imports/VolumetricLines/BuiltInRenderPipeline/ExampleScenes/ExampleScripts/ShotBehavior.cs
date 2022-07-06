d39c388faed8224fdb5a8b318d5637739b25bf28
turbo-potato13 <truba191@yandex.ru>
2022-07-07 02:31:46 +0300

    Сделал лазеры, подправил карту

---
+++ Assets/Imports/VolumetricLines/BuiltInRenderPipeline/ExampleScenes/ExampleScripts/ShotBehavior.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class ShotBehavior : MonoBehaviour
+{
+    private Vector3 m_target;
+    public float speed;
+
+    void Update()
+    {
+        // transform.rotation = new Quaternion(0, 0, 0 ,0);
+        transform.position +=
+            transform.forward * Time.deltaTime * 300f; // The step size is equal to speed times frame time.
+        float step = speed * Time.deltaTime;
+
+        if (m_target != null)
+        {
+            if (transform.position == m_target)
+            {
+                Destroy(gameObject);
+            }
+
+            transform.position = Vector3.MoveTowards(transform.position, m_target, step);
+            Destroy(gameObject, 1f);
+        }
+    }
+
+    public void setTarget(Vector3 target)
+    {
+        m_target = target;
+    }
+}
--- Assets/Scripts/LaserGun.cs
+++ Assets/Scripts/LaserGun.cs
@@ -5,29 +5,42 @@ public class LaserGun : MonoBehaviour
     public float damage = 35f;
     public float range = 100f;
     public Camera FPSCamera;
-    public ParticleSystem lazerFlash;
     public GameObject impactEffect;
     public float impactForce = 30f;
+
+    public float shootRate;
+    private float shotRateTimeStamp;
+    public GameObject shotPrefab;
 
     private void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
             shoot();
+            if (Time.time > shotRateTimeStamp)
+            {
+                shotRateTimeStamp = Time.time + shootRate;
+            }
         }
     }
 
     private void shoot()
     {
-        lazerFlash.Play();
         RaycastHit hit;
+
+        GameObject laser = Instantiate(shotPrefab, transform.position, shotPrefab.transform.rotation);
+        ShotBehavior shotBehavior = laser.GetComponent<ShotBehavior>();
         if (Physics.Raycast(FPSCamera.transform.position, FPSCamera.transform.forward, out hit, range))
         {
+            shotBehavior.setTarget(hit.point);
+            Destroy(laser, 1f);
             Debug.Log(hit.transform.name);
+
             Enemy enemy = hit.transform.GetComponent<Enemy>();
             if (enemy != null)
             {
                 enemy.takeDamage(damage);
+                Destroy(laser);
             }
 
             if (hit.rigidbody != null)
@@ -36,7 +49,7 @@ public class LaserGun : MonoBehaviour
             }
 
             GameObject impactGo = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
-            Destroy(impactGo, 2f);
+            Destroy(impactGo, 0.5f);
         }
     }
 }
