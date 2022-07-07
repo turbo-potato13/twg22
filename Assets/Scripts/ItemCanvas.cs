ebc37ae24dba67fea5c6b171c789ea9052e74cd2
turbo-potato13 <truba191@yandex.ru>
2022-07-08 01:10:23 +0300

    Добавил все элементы из таблицы

--- Assets/Scripts/ItemCanvas.cs
+++ Assets/Scripts/ItemCanvas.cs
@@ -7,6 +7,12 @@ public class ItemCanvas : MonoBehaviour
 {
     public TMP_Text metalCountText;
     public TMP_Text coalCountText;
+    public TMP_Text soilCountText;
+    public TMP_Text waterCountText;
+    public TMP_Text semenCountText;
+    public TMP_Text filterCountText;
+    public TMP_Text sunPanelBrokenCountText;
+    public TMP_Text sunPanelCountText;
 
     public void setCount(Dictionary<String, int> items)
     {
@@ -17,5 +23,29 @@ public class ItemCanvas : MonoBehaviour
         int coalCount = 0;
         items.TryGetValue("уголь", out coalCount);
         coalCountText.text = "Уголь: " + coalCount + " шт.";
+
+        int soilCount = 0;
+        items.TryGetValue("почва", out soilCount);
+        soilCountText.text = "Почва: " + soilCount + " шт.";
+
+        int waterCount = 0;
+        items.TryGetValue("вода", out waterCount);
+        waterCountText.text = "Вода: " + waterCount + " шт.";
+
+        int semenCount = 0;
+        items.TryGetValue("семена", out semenCount);
+        semenCountText.text = "Семена: " + semenCount + " шт.";
+
+        int filterCount = 0;
+        items.TryGetValue("фильтр", out filterCount);
+        filterCountText.text = "Фильтр: " + filterCount + " шт.";
+
+        int sunPanelBrokenCount = 0;
+        items.TryGetValue("солнечныеХ", out sunPanelBrokenCount);
+        sunPanelBrokenCountText.text = "Солнечные\n батареи (слом): " + sunPanelBrokenCount + " шт.";
+
+        int sunPanelCount = 0;
+        items.TryGetValue("солнечные", out sunPanelCount);
+        sunPanelCountText.text = "Солнечные батареи: " + sunPanelCount + " шт.";
     }
 }
